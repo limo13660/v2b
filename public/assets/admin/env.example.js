c4bfdb58f81e2ad41a5c058f7a2d847e6ce453af
limo13660 <wancnmbd@outlook.com>
2024-06-20 20:48:17 +0800

    1

---
+++ public/assets/admin/env.example.js
@@ -0,0 +1,17 @@
+window.settings = {
+  // 站点标题
+  title: 'DaoTech',
+  // API
+  host: '',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: '',
+  logo: '',
+  // 需与DaoTech设置中的后台路径一致
+  secure_path: 'admin'
+}
