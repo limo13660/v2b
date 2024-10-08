b34379d84aa6ee8e8fdbe8fb2a654084e8142e66
limo13660 <wancnmbd@outlook.com>
2024-10-08 22:20:09 +0800

    适配开心主题

--- app/Http/Controllers/V1/User/ServerController.php
+++ app/Http/Controllers/V1/User/ServerController.php
@@ -3,31 +3,90 @@
 namespace App\Http\Controllers\V1\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\NodeResource;
 use App\Models\User;
 use App\Services\ServerService;
 use App\Services\UserService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\DB;
+
+/**kxboard主题 */
+use App\Protocols\General;
 
 class ServerController extends Controller
 {
+
+    // public function fetch(Request $request)
+    // {
+    //     $user = User::find($request->user['id']);
+    //     $servers = [];
+    //     $userService = new UserService();
+    //     if ($userService->isAvailable($user)) {
+    //         $servers = ServerService::getAvailableServers($user);
+    //     }
+    //     $eTag = sha1(json_encode(array_column($servers, 'cache_key')));
+    //     if (strpos($request->header('If-None-Match'), $eTag) !== false ) {
+    //         return response(null,304);
+    //     }
+    //     $data = NodeResource::collection($servers);
+    //     return response([
+    //         'data' => $data
+    //     ])->header('ETag', "\"{$eTag}\"");
+    // }
+
     public function fetch(Request $request)
     {
         $user = User::find($request->user['id']);
         $servers = [];
         $userService = new UserService();
+
         if ($userService->isAvailable($user)) {
-            $serverService = new ServerService();
-            $servers = $serverService->getAvailableServers($user);
-        }
-        $eTag = sha1(json_encode(array_column($servers, 'cache_key')));
-        if (strpos($request->header('If-None-Match'), $eTag) !== false ) {
-            abort(304);
+            $servers = ServerService::getAvailableServers($user);
         }
 
+        /**kxboard主题 每个节点生成配置 */
+        if($request->input('type')=='config'){
+            // $servers = ServerService::getAvailableServers($user);
+            $servers = json_decode(json_encode($servers), true);
+            $servers = array_map(function ($server) use($user) {
+                $class = new General($user, $server);
+                $server['config'] = $class->handle();;
+                return $server;
+            }, $servers);
+            $servers = NodeResource::collection($servers);
+            return response(['data' => $servers]);
+        }
+
+
+        $eTag = sha1(json_encode(array_column($servers, 'cache_key')));
+        if (strpos($request->header('If-None-Match'), $eTag) !== false ) {
+            return response(null,304);
+        }
+        $servers = NodeResource::collection($servers);
         return response([
-            'data' => $servers
+            'data' => $servers,
         ])->header('ETag', "\"{$eTag}\"");
     }
+
+    /**kxboard主题 增加flag图标字段
+     *
+        ALTER TABLE `v2_server_vmess` ADD COLUMN `flag` VARCHAR(255) DEFAULT '' NULL AFTER `id`;
+        ALTER TABLE `v2_server_vless` ADD COLUMN `flag` VARCHAR(255) DEFAULT '' NULL AFTER `id`;
+        ALTER TABLE `v2_server_trojan` ADD COLUMN `flag` VARCHAR(255) DEFAULT '' NULL AFTER `id`;
+        ALTER TABLE `v2_server_shadowsocks` ADD COLUMN `flag` VARCHAR(255) DEFAULT '' NULL AFTER `id`;
+        ALTER TABLE `v2_server_hysteria` ADD COLUMN `flag` VARCHAR(255) DEFAULT '' NULL AFTER `id`;
+    */
+    /**kxboard主题 web的节点列表 */
+    public function config(Request $request)
+    {
+        $user = User::find($request->user['id']);
+
+        $type = $request->input('type');
+        $id = $request->input('id');
+        $server = User::find($request->input('id'));
+
+        $class = new General($user, $server);
+        return response(['data' => $class->handle()]);
+    }
+
+
 }
--- app/Protocols/General.php
+++ app/Protocols/General.php
@@ -2,7 +2,6 @@
 
 namespace App\Protocols;
 
-use App\Utils\Helper;
 
 class General
 {
@@ -18,11 +17,173 @@ class General
 
     public function handle()
     {
-        $uri = '';
+        $servers = $this->servers;
+        $user = $this->user;
 
-        foreach ($this->servers as $server) {
-            $uri .= Helper::buildUri($this->user['uuid'], $server);
+        /**kxboard主题 单个节点配置生成 */
+        $uri = '';
+        if(array_key_exists('id', $servers)){
+            $item = $servers;
+            if ($item['type'] === 'vmess') {
+                $uri .= self::buildVmess($user['uuid'], $item);
+            }
+            if ($item['type'] === 'vless') {
+                $uri .= self::buildVless($user['uuid'], $item);
+            }
+            if ($item['type'] === 'shadowsocks') {
+                $uri .= self::buildShadowsocks($item['password'], $item);
+            }
+            if ($item['type'] === 'trojan') {
+                $uri .= self::buildTrojan($user['uuid'], $item);
+            }
+            return $uri;
+        }
+
+        foreach ($servers as $item) {
+            if ($item['type'] === 'vmess') {
+                $uri .= self::buildVmess($user['uuid'], $item);
+            }
+            if ($item['type'] === 'vless') {
+                $uri .= self::buildVless($user['uuid'], $item);
+            }
+            if ($item['type'] === 'shadowsocks') {
+                $uri .= self::buildShadowsocks($item['password'], $item);
+            }
+            if ($item['type'] === 'trojan') {
+                $uri .= self::buildTrojan($user['uuid'], $item);
+            }
         }
         return base64_encode($uri);
     }
+
+    public static function buildShadowsocks($password, $server)
+    {
+        $name = rawurlencode($server['name']);
+        $str = str_replace(
+            ['+', '/', '='],
+            ['-', '_', ''],
+            base64_encode("{$server['cipher']}:{$password}")
+        );
+        return "ss://{$str}@{$server['host']}:{$server['port']}#{$name}\r\n";
+    }
+
+    public static function buildVmess($uuid, $server)
+    {
+        $config = [
+            "v" => "2",
+            "ps" => $server['name'],
+            "add" => $server['host'],
+            "port" => (string)$server['port'],
+            "id" => $uuid,
+            "aid" => '0',
+            "net" => $server['network'],
+            "type" => "none",
+            "host" => "",
+            "path" => "",
+            "tls" => $server['tls'] ? "tls" : "",
+        ];
+        if ($server['tls']) {
+            if ($server['tlsSettings']) {
+                $tlsSettings = $server['tlsSettings'];
+                if (isset($tlsSettings['serverName']) && !empty($tlsSettings['serverName']))
+                    $config['sni'] = $tlsSettings['serverName'];
+            }
+        }
+        if ((string)$server['network'] === 'tcp') {
+            $tcpSettings = $server['networkSettings'];
+            if (isset($tcpSettings['header']['type'])) $config['type'] = $tcpSettings['header']['type'];
+            if (isset($tcpSettings['header']['request']['path'][0])){
+                $paths = $tcpSettings['header']['request']['path'];
+                $config['path'] = $paths[array_rand($paths)];
+            }
+            if (isset($tcpSettings['header']['request']['headers']['Host'][0])){
+                $hosts = $tcpSettings['header']['request']['headers']['Host'];
+                $config['host'] = $hosts[array_rand($hosts)];
+            }
+        }
+        if ((string)$server['network'] === 'ws') {
+            $wsSettings = $server['networkSettings'];
+            if (isset($wsSettings['path'])) $config['path'] = $wsSettings['path'];
+            if (isset($wsSettings['headers']['Host'])) $config['host'] = $wsSettings['headers']['Host'];
+        }
+        if ((string)$server['network'] === 'grpc') {
+            $grpcSettings = $server['networkSettings'];
+            if (isset($grpcSettings['serviceName'])) $config['path'] = $grpcSettings['serviceName'];
+        }
+        return "vmess://" . base64_encode(json_encode($config)) . "\r\n";
+    }
+
+    public static function buildVless($uuid, $server){
+        $host = $server['host']; //节点地址
+        $port = $server['port']; //节点端口
+        $name = $server['name']; //节点名称
+
+        $config = [
+            'mode' => 'multi', //grpc传输模式
+            'security' => '', //传输层安全 tls/reality
+            'encryption' => 'none', //加密方式
+            'type' => $server['network'], //传输协议
+        ];
+        // 判断是否开启XTLS
+        if($server['flow']) ($config['flow'] = $server['flow']);
+        // 如果开启TLS
+        if ($server['tls']) {
+            switch($server['tls']){
+                case 1:
+                    if ($server['tls_settings']) {
+                        $tlsSettings = $server['tls_settings'];
+                        if (isset($tlsSettings['server_name']) && !empty($tlsSettings['server_name']))
+                            $config['sni'] = $tlsSettings['server_name'];
+                            $config['security'] = "tls";
+                    }
+                    break;
+                case 2: //reality
+                    $config['security'] = "reality";
+                    $tls_settings = $server['tls_settings'];
+                    if(($tls_settings['public_key'] ?? null)
+                    && ($tls_settings['short_id'] ?? null)
+                    && ($tls_settings['server_name'] ?? null)){
+                        $config['pbk'] = $tls_settings['public_key'];
+                        $config['sid'] = $tls_settings['short_id'];
+                        $config['sni'] = $tls_settings['server_name'];
+                        $config['servername'] = $tls_settings['server_name'];
+                        $config['spx'] = "/";
+                        $fingerprints = ['chrome', 'firefox', 'safari', 'ios', 'edge', 'qq']; //随机客户端指纹
+                        $config['fp'] = $fingerprints[rand(0,count($fingerprints) - 1)];
+                    };
+                    break;
+            }
+        }
+        // 如果传输协议为ws
+        if ((string)$server['network'] === 'ws') {
+            $wsSettings = $server['network_settings'];
+            if (isset($wsSettings['path'])) $config['path'] = $wsSettings['path'];
+            if (isset($wsSettings['headers']['Host'])) $config['host'] = $wsSettings['headers']['Host'];
+        }
+        // 传输协议为grpc
+        if ((string)$server['network'] === 'grpc') {
+            $grpcSettings = $server['network_settings'];
+            if (isset($grpcSettings['serviceName'])) $config['serviceName'] = $grpcSettings['serviceName'];
+        }
+
+        $user = $uuid . '@' . $host . ':' . $port;
+        $query = http_build_query($config);
+        $fragment = urlencode($name);
+        $link = sprintf("vless://%s?%s#%s\r\n", $user, $query, $fragment);
+        return $link;
+    }
+
+    public static function buildTrojan($password, $server)
+    {
+        $name = rawurlencode($server['name']);
+        $query = http_build_query([
+            'allowInsecure' => $server['allow_insecure'],
+            'peer' => $server['server_name'],
+            'sni' => $server['server_name']
+        ]);
+        $uri = "trojan://{$password}@{$server['host']}:{$server['port']}?{$query}#{$name}";
+        $uri .= "\r\n";
+        return $uri;
+    }
+
 }
